9d7652c3f9d65f416189ad311d90f38a559b51ad
Leonhard-Euler-ai <873406454@qq.com>
2021-02-10 11:50:31 +0800

    同步项目到supermall中首页轮播图展示之前

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <MainTabBar/>
   </div>
 </template>
 
 <script>
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
 
 export default {
   name: 'app',
   components: {
-
+    MainTabBar
   }
 }
 </script>
 
 <style>
-
+@import "assets/css/base.css";
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                'assets': '@/assets',
-                'common': 'src/common',
-                'components': 'src/components',
-                'network': 'src/network',
-                'views': 'src/views',
-            }
-        }
-    }
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('common', resolve('src/common'))
+      .set('views', resolve('src/views'))
+      .set('components', resolve('src/components'))
+      .set('network', resolve('src/network'))
+  }
 }
