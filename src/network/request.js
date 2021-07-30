a700df3fb4c5149d6ea58546c2f551bc791c699b
Leonhard-Euler-ai <66982603+Leonhard-Euler-ai@users.noreply.github.com>
2021-07-30 20:47:08 +0800

    Update request.js
    修改服务器地址

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
     //创建axios实例
     const instance = axios.create(
         {
-            baseURL: 'http://152.136.185.210:7878/api/m5',
+            baseURL: 'http://123.207.32.32:8000',
             timeout: 5000
         }
     )
